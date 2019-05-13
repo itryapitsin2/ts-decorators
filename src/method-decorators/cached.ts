1d1347e557fc81b068ba7a7451e3e0934f1f31ad
itryapitsin <iliya.tryapisin@gmail.com>
2019-05-13 16:27:17 +0300

    Add cached method decorator

---
+++ src/method-decorators/cached.ts
@@ -0,0 +1,27 @@
+interface CacheItem {
+    value: string;
+    createdDate: Date;
+}
+
+/**
+ *
+ * @param {number} timeout Expired timeout in seconds
+ */
+export function cached(timeout?: number = Infinity) {
+    return function(target: any, _methodName: string, descriptor: PropertyDescriptor) {
+        let method = descriptor.value;
+        const cache: Map<string, CacheItem> = new Map<string, CacheItem>();
+        descriptor.value = function() {
+            const key = JSON.stringify(arguments);
+            if (cache.has(key)) {
+                return cache.get(key).value;
+            }
+
+            const result = Reflect.apply(method, target, arguments);
+            cache.set(key, { value: result, createdDate: new Date() });
+
+            return result;
+        };
+        return descriptor;
+    };
+}
