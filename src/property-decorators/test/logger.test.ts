312c79370dbfc46acb97ea613f7a9c2c056da23a
itryapitsin <iliya.tryapisin@gmail.com>
2019-05-16 10:25:17 +0300

    Merge branch 'master' of https://github.com/itryapitsin2/ts-decorators
    # Conflicts:
    #	src/method-decorators/logger.ts

--- src/property-decorators/test/logger.test.ts
+++ src/property-decorators/test/logger.test.ts
@@ -1,4 +1,4 @@
-import { logger } from '../logger';
+import { logger } from '../';
 
 describe('logger property decorator tests', () => {
     test('test warning in console on getting and setting ', () => {
