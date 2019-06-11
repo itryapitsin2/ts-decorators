efd8ff66ebe779c254e4e353ab5c76fc67809cb3
itryapitsin <iliya.tryapisin@gmail.com>
2019-06-11 10:04:38 +0300

    Improve mixin test

--- src/class-decorators/test/mixin.test.ts
+++ src/class-decorators/test/mixin.test.ts
@@ -1,35 +1,40 @@
-import {mixin} from "../mixin";
+import {mixin} from '../mixin';
 
 describe('mixin decorator tests', () => {
-    test('check multiple inheritance', () => {
-        const caller = jest.fn((msg: string) => {
-            console.log(msg);
+    test('check mixin', () => {
+        const caller = jest.fn((message) => {
+            console.log(message);
         });
 
         class A {
             public methodA(): void {
-                caller("Called A.methodA");
+                caller('Called A.methodA');
             }
         }
 
         class B {
             public methodB(): void {
-                caller("Called B.methodB");
+                caller('Called B.methodB');
             }
             public fieldB: boolean = true;
         }
 
-        interface C extends A, B {
-        }
+        interface C extends A, B {}
         @mixin(A, B)
         class C {
+            public fieldC: string;
         }
 
         const c = new C();
+
+        expect(c.methodA).toBeDefined();
+        expect(c.methodB).toBeDefined();
+        expect(c.fieldB).toBeDefined();
+
         c.methodA();
         c.methodB();
 
         expect(caller).toHaveBeenCalledTimes(2);
         expect(c.fieldB).toEqual(true);
-    })
+    });
 });
